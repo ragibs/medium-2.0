27d726e826bb3cb79193432b51c568ef6b918d8f
Ragib Sina <ragib.sina@gmail.com>
2022-10-29 13:27:46 -0600

    styled header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 function Header() {
   return (
-    <header>
-      <div>
+    <header className="flex justify-between p-5 max-w-7xl mx-auto">
+      <div className="flex items-center space-x-5">
         <Link href="/">
           <img
             className="w-44 object-contain cursor-pointer"
@@ -11,13 +11,20 @@ function Header() {
             alt="medium logo"
           />
         </Link>
-        <div>
+        <div className="hidden md:inline-flex items-center space-x-5">
           <h3>About</h3>
           <h3>Contact</h3>
-          <h3>Follow</h3>
+          <h3 className="text-white bg-green-600 py-1 px-4 rounded-full">
+            Follow
+          </h3>
         </div>
       </div>
-      <div></div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign In</h3>
+        <h3 className="border px-4 y-1 rounded-full border-green-600">
+          Get Started
+        </h3>
+      </div>
     </header>
   );
 }
