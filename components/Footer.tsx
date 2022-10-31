26dbc58c0362a18f3875b24ee3694a5187d0fba5
Ragib Sina <ragib.sina@gmail.com>
2022-10-31 08:54:32 -0600

    added footer

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <footer className="flex justify-center mt-7 text-yellow-500 pb-2 text-sm">
+      <hr pb-2 />
+      <h3>By Ragib Sina, with Sanity, TypeScript, Next.js and Tailwind CSS</h3>
+    </footer>
+  );
+}
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import { sanityClient, urlFor } from "../sanity";
@@ -46,6 +47,7 @@ export function Home({ posts }: Props) {
           </Link>
         ))}
       </div>
+      <Footer />
     </div>
   );
 }
